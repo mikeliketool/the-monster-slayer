7948bb6acd9854258f05455bea35275ff1958917
Mike Fehrenbach <fehrenbach.mike@gmail.com>
2020-07-01 23:21:42 -0400

    Implement the attack method
    Make sure we display who lost.
    Create reusable functions to check for a win and damage the opponent.

--- src/components/TheMonsterSlayer.vue
+++ src/components/TheMonsterSlayer.vue
@@ -22,14 +22,14 @@
         </div>
       </div>
     </section>
-    <section class="row controls" v-if="!gameIsRunnings">
+    <section class="row controls" v-if="!gameIsRunning">
       <div class="small-12 columns">
         <button id="start-game" @click="startGame">START NEW GAME</button>
       </div>
     </section>
     <section class="row controls" v-else>
       <div class="small-12 columns">
-        <button id="attack">ATTACK</button>
+        <button id="attack" @click="doAttack">ATTACK</button>
         <button id="special-attack">SPECIAL ATTACK</button>
         <button id="heal">HEAL</button>
         <button id="give-up">GIVE UP</button>
@@ -62,6 +62,35 @@ export default {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.displayNames = {
+        'playerHealth': 'You',
+        'monsterHealth': 'Monster'
+      }
+    },
+    damageOpponent(maxDamage, minDamage, opponentHealth) {
+      const damage = Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
+      this[opponentHealth] -= damage;
+    },
+    checkForWin(opponentHealth) {
+      if (this[opponentHealth] <=0 ){
+        if (confirm(`${this.displayNames[opponentHealth]} Lost! Start another game?`)) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+
+      return false;
+    },
+    doAttack() {
+      this.damageOpponent(10, 3, 'monsterHealth');
+      if (this.checkForWin('monsterHealth')) {
+        return;
+      }
+
+      this.damageOpponent(12, 5, 'playerHealth');
+      this.checkForWin('playerHealth');
     }
   }
 }
